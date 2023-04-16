b484649342d89295122641dc25228c1c93968d91
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-16 18:04:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+const team = players;
+const team1 =[];
+players.forEach((element) =>{
+    team1.push(element);
+});
+
 window.players = players;
 window.person = person;
 window.team = team;
